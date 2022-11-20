02612845f3c11bcc195adfbe4047d97e754ce02a
akshaya-sr-03 <118654131+akshaya-sr-03@users.noreply.github.com>
2022-11-20 13:02:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+<link
+href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" 
+rel="stylesheet"  integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+crossorigin="anonymous"
+/>
+
+<i class="fa fa-angle-down"></i>
+<i class="fa fa-ellipsis-v"></i>
+<i class="fa fa-heart fav"></i>
+<i class="fa fa-play-circle"></i>
+<i class="fa fa-step-forward"></i>
+<i class="fa fa-step-backward"></i>
